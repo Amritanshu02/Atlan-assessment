db6347d0a956e26432420229ae43d396b9860b9f
Amritanshu02 <78687085+Amritanshu02@users.noreply.github.com>
2023-10-06 19:45:34 +0530

    Added Predefined Queries Section

---
+++ src/components/DemoTable/DemoTable.jsx
@@ -0,0 +1,46 @@
+import * as React from 'react';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import "./DemoTable.css";
+import { rows } from "../../data";
+
+function DemoTable() {
+    return (
+        <TableContainer component={Paper}>
+            <Table sx={{ minWidth: 650 }} size="small" aria-label="a dense table">
+                <TableHead>
+                    <TableRow>
+                        <TableCell className="thead-data">Dessert (100g serving)</TableCell>
+                        <TableCell className="thead-data" align="right">Calories</TableCell>
+                        <TableCell className="thead-data" align="right">Fat&nbsp;(g)</TableCell>
+                        <TableCell className="thead-data" align="right">Carbs&nbsp;(g)</TableCell>
+                        <TableCell className="thead-data" align="right">Protein&nbsp;(g)</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {rows.map((row) => (
+                        <TableRow
+                            key={row.name}
+                            sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                        >
+                            <TableCell component="th" scope="row">
+                                {row.name}
+                            </TableCell>
+                            <TableCell align="right">{row.calories}</TableCell>
+                            <TableCell align="right">{row.fat}</TableCell>
+                            <TableCell align="right">{row.carbs}</TableCell>
+                            <TableCell align="right">{row.protein}</TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
+        </TableContainer>
+    );
+}
+
+export default DemoTable;
